3cd032230c13a2ff37a55a6931cdd2a8412ed092
devbenjasx98 <benjasanchez175@gmail.com>
2026-02-01 16:10:55 -0700

    added DB mongo

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // Set a global prefix for all routes
+  app.setGlobalPrefix('api/v2');
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
