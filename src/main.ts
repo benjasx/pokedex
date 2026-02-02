cf1402befe4e4d51c8bf0617cf757817daad8527
devbenjasx98 <benjasanchez175@gmail.com>
2026-02-02 10:57:49 -0700

    insertando datos en DB

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Set a global prefix for all routes
   app.setGlobalPrefix('api/v2');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        exposeUnsetFields: false,
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
