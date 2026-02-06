f44c72f8261ca5f353836dfb17bc0bd4d855915f
devbenjasx98 <benjasanchez175@gmail.com>
2026-02-06 11:23:08 -0700

    Validacio de variables de entorno

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,5 +2,5 @@ export const EnvConfiguration = () => ({
   eviroment: process.env.NODE_ENV || 'dev',
   mongoDB: process.env.MONGODB,
   port: process.env.PORT || 3000,
-  defaultLimit: process.env.DEFAULT_LIMIT || 10,
+  defaultLimit: +process.env.DEFAULT_LIMIT! || 10,
 });
